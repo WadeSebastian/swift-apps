ced1abf06844da52c46d5aafbd322766a35022b1
WadeSebastian <wade.s@etoncollege.org.uk>
2020-01-13 21:42:44 +0000

    Tutorial part 2 complete - Switches for different packages and more cars to choose from. Also a button to change cars. :)

---
+++ CarCustomizer/CarCustomizer/StarterCars.swift
@@ -0,0 +1,13 @@
+//
+//  StarterCars.swift
+//  CarCustomizer
+//
+//  Created by Wade, Sebastian (WING) on 12/01/2020.
+//  Copyright © 2020 Wade, Sebastian (WING). All rights reserved.
+//
+
+    import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 140, acceleration: 7.3, handling: 5), Car(make: "Volkswagen", model: "Golf GTI", topSpeed: 165, acceleration: 5.6, handling: 4), Car(make: "Mini", model: "Cooper S", topSpeed: 130, acceleration: 7.1, handling: 7), Car(make: "Audi", model: "S3 Performance", topSpeed: 155, acceleration: 4.5, handling: 6)]
+}
--- CarCustomizer/CarCustomizer/ViewController.swift
+++ CarCustomizer/CarCustomizer/ViewController.swift
@@ -10,16 +10,127 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var engineAndExhaustPackage: UISwitch!
+    @IBOutlet var tiresPackage: UISwitch!
+    @IBOutlet var ecuAndFuelPackage: UISwitch!
+    @IBOutlet var fmRadioPackage: UISwitch!
     @IBOutlet weak var carStatistics: UILabel!
+    @IBOutlet var remainingFundsDisplay: UILabel!
     
-    var car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    var remainingFunds = 0 {
+        didSet {
+            remainingFundsDisplay.text = "Remaining Funds: \(remainingFunds)"
+            disableUnaffordablePackages()
+        }
+    }
+    var starterCars = StarterCars()
+    var currentCarIndex = 0
+    var car: Car? {
+        didSet {
+            carStatistics.text = car?.displayStats()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        carStatistics.text = car.displayStats()
+        car = starterCars.cars[currentCarIndex]
+        reset()
     }
-
-
+    
+    func reset() {
+        remainingFunds = 1_000
+        engineAndExhaustPackage.setOn(false, animated: true)
+        tiresPackage.setOn(false, animated: true)
+        ecuAndFuelPackage.setOn(false, animated: true)
+        fmRadioPackage.setOn(false, animated: true)
+    }
+    
+    func disableUnaffordablePackages() {
+        engineAndExhaustPackage.isEnabled = shouldBeEnabled(engineAndExhaustPackage)
+        tiresPackage.isEnabled = shouldBeEnabled(tiresPackage)
+        ecuAndFuelPackage.isEnabled = shouldBeEnabled(ecuAndFuelPackage)
+        fmRadioPackage.isEnabled = shouldBeEnabled(fmRadioPackage)
+    }
+    
+    func shouldBeEnabled(_ chosenSwitch: UISwitch) -> Bool {
+        if chosenSwitch.isOn {
+            return true
+        } else {
+            if remainingFunds >= 750 {
+                return true
+            } else if remainingFunds >= 500 {
+                let switchName = chosenSwitch.accessibilityIdentifier
+                if switchName == "ecuAndFuelPackageSwitch" {
+                    return false
+                } else {
+                    return true
+                }
+            } else if remainingFunds >= 250 {
+                let switchName = chosenSwitch.accessibilityIdentifier
+                if switchName == "fmRadioPackageSwitch" {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+    }
+    
+    @IBAction func changeCar(_ sender: Any) {
+        currentCarIndex = currentCarIndex + 1
+        if currentCarIndex >= starterCars.cars.count {
+                currentCarIndex = 0
+        }
+        car = starterCars.cars[currentCarIndex]
+        reset()
+    }
+    
+    
+    @IBAction func engineAndExhaustToggle(_ sender: Any) {
+        if engineAndExhaustPackage.isOn {
+            car?.topSpeed += 10
+            remainingFunds -= 500
+        } else {
+            car?.topSpeed -= 10
+            remainingFunds += 500
+        }
+    }
+    
+    @IBAction func tiresPackageToggle(_ sender: Any) {
+        if tiresPackage.isOn {
+            car?.handling += 1
+            remainingFunds -= 500
+        } else {
+            car?.handling -= 1
+            remainingFunds += 500
+        }
+    }
+    
+    @IBAction func ecuAndFuelPackageToggle(_ sender: Any) {
+        if ecuAndFuelPackage.isOn {
+            car?.acceleration += 2
+            car?.topSpeed += 15
+            remainingFunds -= 750
+        } else {
+            car?.acceleration -= 2
+            car?.topSpeed -= 15
+            remainingFunds += 750
+        }
+    }
+    
+    @IBAction func fmRadioPackageToggle(_ sender: Any) {
+        if fmRadioPackage.isOn {
+            car?.topSpeed += 5
+            car?.acceleration += 1
+            remainingFunds -= 250
+        } else {
+            car?.topSpeed -= 5
+            car?.acceleration -= 1
+            remainingFunds += 250
+        }
+    }
+    
 }
 
