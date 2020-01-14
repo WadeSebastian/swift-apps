16169b3d4736a6560bf1de8c293a4ab0d1e32082
WadeSebastian <wade.s@etoncollege.org.uk>
2020-01-14 12:26:27 +0000

    In the beginning was the tamagotchi... a brave new world. (The start of the iOS app without anything added yet)

---
+++ tamagotchiApp/tamagotchiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tamagotchiApp
+//
+//  Created by Wade, Sebastian (WING) on 14/01/2020.
+//  Copyright © 2020 Wade, Sebastian (WING). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
