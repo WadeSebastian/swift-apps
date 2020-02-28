e3c4f83f6f115776cc71cfd60841928dc86a1aee
WadeSebastian <wade.s@etoncollege.org.uk>
2020-02-28 22:48:39 +0000

    Tutorial 1 finished (location and geocoder formatting)

--- MusicByLocation/MusicByLocation/ViewController.swift
+++ MusicByLocation/MusicByLocation/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    
+    @IBOutlet var musicRecommendations: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
     }
 
-
+    @IBAction func findMusic(_ sender: Any) {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let firstLocation = locations.first {
+            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in if error != nil {
+                    self.musicRecommendations.text = firstLocation.coordinate.latitude.description
+                } else {
+                let currentLocation = placemarks?[0]
+                self.musicRecommendations.text = """
+                Country: \(currentLocation?.country ?? "COUNTRY UNAVAILABLE")
+                Province/State: \(currentLocation?.administrativeArea ?? "STATE UNAVAILABLE")
+                County: \(currentLocation?.subAdministrativeArea ?? "COUNTY UNAVAILABLE")
+                City: \(currentLocation?.locality ?? "CITY UNAVAILABLE")
+                
+                Name: \(currentLocation?.name ?? "NAME UNAVAILABLE")
+                Postcode: \(currentLocation?.postalCode ?? "POSTCODE UNAVAILABLE")
+                """
+                }
+            })
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        musicRecommendations.text = "Could not acces user's location. Error: \(error.localizedDescription)"
+    }
+    
 }
 
